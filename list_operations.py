8c36c750928fb1ed82e1579d0bf7dadebec7005c
Your Name <you@example.com>
2020-01-09 12:31:48 -0800

    Added replace middle function's return value

--- list_operations.py
+++ list_operations.py
@@ -161,7 +161,7 @@ def replace_head(input_list):
 
     """
     input_list[0] = 42
-    pass
+    
 
 
 def replace_third_and_last(input_list):
@@ -177,7 +177,7 @@ def replace_third_and_last(input_list):
     """
     input_list[2] = 37
     input_list[-1] = 37
-    pass
+
 
 
 def replace_middle(input_list):
@@ -195,8 +195,8 @@ def replace_middle(input_list):
     True
 
     """
+    input_list[2:-2] = [42, 37]
 
-    pass
 
 
 def delete_third_and_seventh(input_list):
